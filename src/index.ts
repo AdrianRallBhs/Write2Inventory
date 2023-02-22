b8f7a1bb797f5d0a7043b107d59b7119b7634326
Adrian Rall <adrian@asda4.de>
2023-02-22 11:59:50 +0100

    take dotnet installation to action

--- src/index.ts
+++ src/index.ts
@@ -538,10 +538,10 @@ sources.forEach(element => {
 
   (async () => {
   const packages =  await getNugetPackagesInfo();
-  console.log(JSON.stringify(packages, null, 2));
+//   console.log(JSON.stringify(packages, null, 2));
 packages.forEach(element => {
     let packJson = JSON.stringify(element, null, 2)
-    core.info(packJson)
+    // core.info(packJson)
 });
 })();
 
