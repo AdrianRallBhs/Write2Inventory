43d7f01e33aa10ed382948d43afd7e277696feeb
Adrian Rall <adrian@asda4.de>
2023-02-23 21:39:53 +0100

    false nugetpackagemethod?

--- src/index.ts
+++ src/index.ts
@@ -328,13 +328,17 @@ const NugetPackageInfos: NugetPackageInfo[] = [];
 
 (async () => {
 
-const projectList = ['./Blazor4/BlazorApp4/BlazorApp4/BlazorApp4.csproj', './submarine/BlazorSubmarine/BlazorSubmarine/BlazorSubmarine.csproj'];
-const sourceList = ['https://api.nuget.org/v3/index.json'];
+    const dotNetProjects: string[] =  await findALLCSPROJmodules();
+    const ListOfSources: string[] = await getDotnetSources();
+
+// const projectList = ['./Blazor4/BlazorApp4/BlazorApp4/BlazorApp4.csproj', './submarine/BlazorSubmarine/BlazorSubmarine/BlazorSubmarine.csproj'];
+// const sourceList = ['https://api.nuget.org/v3/index.json'];
 
 //const results = await getAllNugetPackages(projectList, sourceList);
 
 
-const NugetPackageInfos = await getOutdatedPackages(projectList, sourceList);
+//const NugetPackageInfos = await getOutdatedPackages(dotNetProjects, ListOfSources);
+const NugetPackageInfos = await getAllNugetPackages(dotNetProjects, ListOfSources);
 console.log(JSON.stringify(NugetPackageInfos, null, 2));
 })();
 
